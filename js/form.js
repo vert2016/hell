a5ec8ba6330adbe940eae3ebc9a1f395d873fe57
marta bobryk <marta.bzl@gmail.com>
2016-11-21 11:51:00 +0000

    Initials

--- js/form.js
+++ js/form.js
@@ -1,21 +1,22 @@
 $(function() {
     $('#form_button').click(function(e) {
-        e.preventDefault();
-        $.ajax({
-                url: "https://formspree.io/marta.bzl@gmail.com",
-                method: "POST",
-                data: {
-                    userName: $('#userName').val(),
-                    email: $('#email').val(),
-                    phone: $('#phone').val()
-                },
-                dataType: "json"
-            })
-            .done(function() {
-                $('form').html('<h1>Thank you!</h1>')
-            })
-            .fail(function() {
-                $('form').html('<h1>Error</h1>')
-            });
+        if ($('form')[0].checkValidity()) {
+            e.preventDefault();
+            $.ajax({
+                    url: "https://formspree.io/marta.bzl@gmail.com",
+                    method: "POST",
+                    data: {
+                        userName: $('#userName').val(),
+                        email: $('#email').val(),
+                        phone: $('#phone').val()
+                    },
+                    dataType: "json"
+                })
+                .done(function() {
+                    $('form').html('<h1>Thank you!</h1>')
+                })
+        }   else .fail(function() {
+            $('form').html('<h1>Error</h1>')
+        });
     });
 });
--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
-$(document).ready(function(){
-    $("#open_quiz").click(function(){
-        $("#quiz_panel").slideToggle("slow")(function(){
-            $("#arrow").rotate({angle:45});
+$(document).ready(function () {
+    $('#open_quiz').click(function () {
+        $('#quiz_panel').slideToggle('slow')(function(){
+            $('#arrow').rotate({angle: 45});
         });
-        
     });
 });
